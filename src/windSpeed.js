b154080fb3f13c0b9d6685ea12e9d0693d8fdfb1
pitazzo <pedromalo@live.com>
2019-11-20 10:46:48 +0100

    Minor changes

--- src/windSpeed.js
+++ src/windSpeed.js
@@ -1,17 +1,32 @@
-var stoch = require('stochastic')
+const stoch = require('stochastic')
+const kmeans = require('node-kmeans');
 const { Household } = require('./dbModels')
 
-function updateCluster(households, mean, deviation, depth){
-    if(depth == X || households.lentgh == 1){
-
+function updateClusters(households, mean, deviation, depth, nClusters = 2){
+    if(depth == 6 || households.lentgh == 1){
+        //update mu and sigma
     }else{
-        //apply k mean
-        //For each cluster
-        //  - generate mean and deviation
-        //  - updateCluster(households, mean, deviation, depth)
+        let positions = Array()
+        for (let i = 0; i < households.length; i++) {
+            positions[i] = [ households[i].coords.x , households[i].coords.y ];
+        }
+        const means = stoch.norm(mean, deviation, nClusters)
+        const deviations = stoch.norm(mean/4, deviation/4, nClusters)
+        kmeans.clusterize(positions, {k:nClusters}, function (error, results){
+            if(error != null){
+                for(let i = 0; i < results.length; i++){
+                    let newHouseholds = Array()
+                    for (let i = 0; i < results.clusterInd.length; i++){
+                        newHouseholds[i] = households[clusterInd[i]]
+                    }
+                    updateClusters(newHouseholds, means[i], deviations[i], depth+1)
+                }
+            }else{
+                console.log('Error while clustering in updateClusters()')
+            }
+        })
     }
 }
-
 
 module.exports = {
     getWindSpeed: async function (householdID){
@@ -19,12 +34,10 @@ module.exports = {
         return stoch.norm(household.windSimulation.mu, household.windSimulation.sigma, 1)
     },
 
-    updateWindParameter: function (){
-        //updateCluster()
+    updateWindParameter: async function (){
+        const households = await Household.find()
+        const mu = (Math.random() * (1000.0 - 100.0) + 100.0)
+        const sigma = (Math.random() * (1000.0 - 100.0) + 100.0)
+        updateClusters(households, mu, sigma, 0)
     }
 }
-
-
-
-
-
